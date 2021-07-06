af677e2fb0de5f4a2f776bdebc85c265c8d5895b
Curle <curle@gemwire.uk>
2021-07-06 03:06:49 +0100

    Initial stub sysroot

---
+++ common/include/stdlib.h
@@ -0,0 +1,13 @@
+#pragma once
+#include <stddef.h>
+
+void  abort(void);
+int   atexit(void (*)(void));
+
+int   atoi(const char*);
+
+void  free(void*);
+
+char* getenv(const char*);
+
+void* malloc(size_t);
